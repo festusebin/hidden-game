e6c50c09f73be0958ba108a841c723084dbb9322
Joseph Ebin <josefophe@gmail.com>
2022-04-29 13:52:02 +0100

    Add files via upload

---
+++ client/src/pages/views/MemberViews2.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import VoterViews from './VoterViews';
+
+const exports = {...VoterViews};
+
+exports.Wrapper = class extends React.Component {
+  render() {
+    const {content} = this.props;
+    return (
+      <div className="Attacher">
+        <h2>Attacher (Charlie)</h2>
+        {content}
+      </div>
+    );
+  }
+}
+
+exports.Secure = class extends React.Component {
+  render() {
+    const {parent} = this.props;
+    const {ctcInfoStr} = this.state || {};
+    return (
+      <div>
+        Please enter the transaction info that was sent to you by your Deployer:
+        <br />
+        <textarea spellcheck="false"
+          className='ContractInfo'
+          onChange={(e) => this.setState({ctcInfoStr: e.currentTarget.value})}
+          placeholder='{}'
+        />
+        <br />
+        <button
+          disabled={!ctcInfoStr}
+          onClick={() => parent.attach(ctcInfoStr)}
+        >Attach</button>
+      </div>
+    );
+  }
+}
+
+exports.Securing = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Attaching, please wait...
+      </div>
+    );
+  }
+}
+
+exports.AcceptStake = class extends React.Component {
+  render() {
+    const {stake, standardUnit, parent} = this.props;
+    const {disabled} = this.state || {};
+    return (
+      <div>
+        The terms of the game are:
+        <br /> stake: {stake} {standardUnit}
+        <br />
+        <button
+          disabled={disabled}
+          onClick={() => {
+            this.setState({disabled: true});
+            parent.termsAccepted();
+          }}
+        >Accept terms and Stake!</button>
+      </div>
+    );
+  }
+}
+
+exports.WaitingForTurn = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Waiting for other player...
+        <br />
+        {/**
+        ** Open the chat in the new tab to discuss more!
+        **/}
+      </div>
+    );
+  }
+}
+
+export default exports;
